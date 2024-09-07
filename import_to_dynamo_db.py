6fb038bc3116974308a86284d68acfa69b953833
Sushil Prasad <20680132+cloudintro@users.noreply.github.com>
2024-09-07 16:44:26 +0530

    updated script to create devide result
    Create result for students and subjects

--- import_to_dynamo_db.py
+++ import_to_dynamo_db.py
@@ -1,25 +1,42 @@
 import json
 
-def write_to_file(file_data, name):
-    target_file_name = f'class_test_result_{name}.json'
-    print(f'Writing to {target_file_name}')
-    with open(target_file_name, 'w') as target_file:
-        file_data_new = {'key' : f'CLASS_TEST_{name}', 'data' : file_data}
-        target_file.write(json.dumps(file_data_new))
-        target_file.close()
+_source_file_name = 'class_test_result_all.json'
+
+def write_to_file(result_dict, result_type):
+    count = 0
+    for name, result in result_dict.items():
+        target_file_name = f'{result_type}_result_{name}.json'
+        print(f'Writing to {target_file_name}')
+        with open(target_file_name, 'w') as target_file:
+            file_data = {result_type : name, 'result' : result}
+            target_file.write(json.dumps(file_data))
+            target_file.close()
+        count+=1
+    print(f'Total {count} {result_type} result created')
+
+def build_result_dict(result, result_dict, result_key):
+    name = f'{result[result_key]}'
+    if result_dict.get(name) is not None:
+        result_dict[name] = result_dict[name] + [result]
+    else:
+        result_dict[name] = [result]
 
 def read_from_file():
-    source_file_name = 'class_test_result_all.json'
-    print('Reading from {source_file_name}')
-    with open(source_file_name, 'r+') as source_file:
+    print(f'Reading from {_source_file_name}')
+    with open(_source_file_name, 'r+') as source_file:
         file_data = json.load(source_file)
         print(file_data)
-        count = 1
-        for data in file_data.get('data'):
-            write_to_file(data, data['name'])
+        subject_result_dict = {}
+        student_result_dict = {}
+        count = 0
+        for result in file_data.get('result'):
+            build_result_dict(result, subject_result_dict, 'subject')
+            build_result_dict(result, student_result_dict, 'name')
             count+=1
-        print(f'Total {count} result created')
         source_file.close()
+        print(f'Total {count} result found in {_source_file_name}')
+    write_to_file(subject_result_dict, 'subject')
+    write_to_file(student_result_dict, 'student')
 
 if __name__ == '__main__':
     read_from_file()
